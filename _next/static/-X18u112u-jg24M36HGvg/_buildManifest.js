e53ed0aeca813a7c560cc97e6031410c5c3998b4
abhinavk454 <32016468+abhinavk454@users.noreply.github.com>
2021-11-24 17:33:46 +0000

    Deploying to gh-pages from @ abhinavk454/ci_test_02@da008cd81d0e6458dc672f437e28e1f9d7f23903 🚀

---
+++ _next/static/-X18u112u-jg24M36HGvg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e36e9ca00e00d53e.css","static/chunks/pages/index-f1a03b703d2e4fcc.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
